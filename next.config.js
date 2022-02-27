ffe4c502f40d414a4ac8196a1611908d10706eb9
Dev_coder <emmanuelomoiya6@gmail.com>
2022-02-27 23:30:04 +0100

    initial

--- next.config.js
+++ next.config.js
@@ -17,8 +17,6 @@ module.exports = {
             // don't resolve 'mongodb' module on the client to prevent this error on build --> Error: Can't resolve 'mongodb'
             config.resolve.fallback = {
                 mongodb: false,
-                axios: false,
-                process: false
             }
         }
 
