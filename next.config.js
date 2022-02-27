70d3642c26fe957a917b9f31080352da3553297a
Dev_coder <emmanuelomoiya6@gmail.com>
2022-02-27 22:30:14 +0100

    initial

--- next.config.js
+++ next.config.js
@@ -12,30 +12,16 @@ module.exports = {
   },
 }
 module.exports = {
-<<<<<<< HEAD
-  webpack: (config, { isServer }) => {
-      if (!isServer) {
-          // don't resolve 'mongodb' module on the client to prevent this error on build --> Error: Can't resolve 'mongodb'
-          config.resolve.fallback = {
-              mongodb: false,
-              axios: false
-          }
-      }
-
-      return config;
-  }
-}
-=======
     webpack: (config, { isServer }) => {
         if (!isServer) {
             // don't resolve 'mongodb' module on the client to prevent this error on build --> Error: Can't resolve 'mongodb'
             config.resolve.fallback = {
                 mongodb: false,
-                axios: false
+                axios: false,
+                process: false
             }
         }
 
         return config;
     }
 }
->>>>>>> dc3b2021061a94f28e7a44a63cb5a300d30fce99
