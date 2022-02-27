7159575ea3abe4074da3ff026c72ebb4e1d30680
EmmanuelOmoiya <79797604+EmmanuelOmoiya@users.noreply.github.com>
2022-02-27 21:27:55 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,3 +11,15 @@ module.exports = {
     ignoreDuringBuilds: true,
   },
 }
+module.exports = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'mongodb' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                mongodb: false
+            }
+        }
+
+        return config;
+    }
+}
