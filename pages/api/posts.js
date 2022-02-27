ddf381d3ad6e4bfe699a26e32b11b86549c4eb40
Dev_coder <emmanuelomoiya6@gmail.com>
2022-02-28 00:48:04 +0100

    initial

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,5 +1,5 @@
 //const { connectToDatabase } = require('../../lib/mongodb');
-import { connectToDatabase } from '../../mongodb';
+import { connectToDatabase } from '../../lib/mongodb';
 
 export default async function handler(req, res) {
     // switch the methods
