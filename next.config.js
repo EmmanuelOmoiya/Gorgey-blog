f3ba5a9532494d63c3b4ea49ec3c6e6cc4cfe871
Dev_coder <emmanuelomoiya6@gmail.com>
2022-02-27 23:43:39 +0100

    initial

--- next.config.js
+++ next.config.js
@@ -1,25 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
 module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
-module.exports = {
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            // don't resolve 'mongodb' module on the client to prevent this error on build --> Error: Can't resolve 'mongodb'
-            config.resolve.fallback = {
-                mongodb: false,
-            }
-        }
+  webpack: (config, { isServer }) => {
+      if (!isServer) {
+          // don't resolve 'mongodb' module on the client to prevent this error on build --> Error: Can't resolve 'mongodb'
+          config.resolve.fallback = {
+              mongodb: false,
+          }
+      }
 
-        return config;
-    }
+      return config;
+  }
 }
